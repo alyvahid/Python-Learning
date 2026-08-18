866938af78eaff67e01d1ab381fe4e35bf5814fa
alyvahid <alyvahid@gmail.com>
2026-08-18 18:12:25 +0500

    Add datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+# Integer
+a = 10
+print("Type of a:", type(a)) 
+
+# Float
+b = 10.5
+print("Type of b:", type(b))
+
+# complex
+c = 2 + 3j
+print("Type of c:", type(c))
+
+# Boolean
+d = True
+print("Type of d:", type(d))
+
+e = False
+print("Type of e:", type(e))    
+
+# SET
+g = {1, 2, 3, 4, 5}
+# G = {'A','b' 'c'}
+print("Type of g:", type(g))
+
+# DICTIONARY
+h = {"Alisha":"25"}
+print("Type of h:", type(h))
+
+# List
+i = [1, 2, 3, 4, 5]
+print("Type of i:", type(i))
+
+# string
+j = "Hello Alisha"
+print("Type of j:", type(j))
+
+# Tuple
+k = (1, 2, 3, 4, 5)
+print("Type of k:", type(k))
