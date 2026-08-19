21cf6d0b30860c0961cec3d884ebdf1c4df28b06
alyvahid <alyvahid@gmail.com>
2026-08-19 14:08:27 +0500

    Add Practice

---
+++ assignment05.py
@@ -0,0 +1,38 @@
+# 1) Write a program to print a string "Hello Python".
+print("Hello Python")
+
+# 2) Write a program to store one value in variable and print that variable.
+x = 5
+print("Print a stored variable which is ",x)
+
+# 3) Write a program to take value from user input and print that value.
+a = input("Enter your name: ")
+print(a)
+
+# 4) Write a program to do a sum of two value.
+x=5
+y=9
+print("Sum of two values is ",x+y)
+
+# 5) Write a program to perform division using variables
+x = int(input("Enter any number: "))
+y = int(input("Enter any number: "))
+print("Division of variable is: ", x/y)
+
+# 6) Write a program to print multiple value of variables
+name = "Alisha"
+age = 23
+Profession = "Security Analyst"
+print(name, age, Profession)
+
+# 7) Write a program to find a type of variable
+x = input("Enter something: ")
+print("Type of variable x is: ",type(x))
+y = int(input("Enter an integer: "))
+print("Type of variable y is: ",type(y))
+
+# 8) Write a program to take two integer value from the user and check both the values are same.
+x = int(input("Enter any number: "))
+y = int(input("Enter any number: "))
+print("values are same: ",x==y)
+
