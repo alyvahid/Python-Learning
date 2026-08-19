a2d2b0a7486d3ada0c228b11a4ae2ab58b24b198
alyvahid <alyvahid@gmail.com>
2026-08-19 11:50:33 +0500

    Add Practice

---
+++ variable03.py
@@ -0,0 +1,12 @@
+# creating integers value into variables
+i = 10
+print(i)
+print(type(i))
+
+# its like storing any datatype in a variable and in this way we can store any datatype into a variable.
+
+# Type Conversion / Type Casting
+# means converting one datatype into another datatype.
+a = int(input("Enter any number: "))
+print(a)
+print(type(a))
